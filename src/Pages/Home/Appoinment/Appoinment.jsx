5de42c12454683a85898d89ea42dcae7ce1900cf
murali.chittoji <murali.chittoji@otsi.co.in>
2023-07-04 11:18:57 +0530

    fixed minor bugs

--- src/Pages/Home/Appoinment/Appoinment.jsx
+++ src/Pages/Home/Appoinment/Appoinment.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import './Appoinment.css';
 import emailjs from '@emailjs/browser'
 
@@ -9,12 +8,7 @@ const Appoinment = (props) => {
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
     const [message, setMessage] = useState("");
-    // const [config, setConfig] = useState({
-    //     userName: "cskrishna@gmail.com",
-    //     password: "F664B9E115AF78A3A269882E5E9DA0BE420C",
-    //     host: "smtp.elasticemail.com",
-    //     port: 2525,
-    // })
+
 
     const sendEmail = (event) => {
         event.preventDefault();
@@ -25,17 +19,14 @@ const Appoinment = (props) => {
             Body: phone,
         }
         console.log(emailInfo)
-        // if(window.Email) {
-        //     window.Email.send(emailInfo).then((message) => {
-        //         alert(message)
-        //     })
-        // }
         emailjs.send("service_xgg695s","template_v97mxwj",{
             from_name: name,
             to_name: "kadali",
-            message: message + ", My Mobile Number is +91" + phone,
+            message: message + ", My Mobile Number is +91" + phone + " My Email id: " + email,
             }, "MfUwZpiWLQ-d-QCEw");
+        props.onClose()
     }
+
     return (
         <section className="appoinment-wrapper">
             <Container>
@@ -43,28 +34,22 @@ const Appoinment = (props) => {
                     <Col sm={12} md={12}>
                         <div className='row'>
                             <div className='col-11 col-sm-9'>
-                            <div className="">
-                            <h1 className="mt-5 text-left pt-2">Request Appointment</h1>
-                        </div>
+                                <div className="">
+                                    <h1 className="mt-5 text-left pt-2">Request Appointment</h1>
+                                </div>
                             </div>
                             <div className='col-1 col-sm-3'>
-                            <button className='mt-5 btn_cross' onClick={() => props.onClose()}>x</button>
+                                <button className='mt-5 btn_cross' onClick={() => props.onClose()}>x</button>
                             </div>
                         </div>
                      
                         <div className="appoinment-form">
                             <form action="#" className="row1 form_pad" onSubmit={sendEmail}>
-                                    <input type="text" placeholder="Name"  value={name} onChange={(e) => setName(e.target.value)} />
-                                    <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                                    <input type="phone" placeholder="Phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
-                                    <input type="text" placeholder="Message" value={message} onChange={(e) => setMessage(e.target.value)} />
-                                {/* <Col md={6} lg={6}>
-                                    <input type="text" placeholder="Subject" />
-                                </Col> */}
-                                <Col md={12} lg={12}>
-                                     {/* <textarea name="Message" id="" cols="30" rows="10" placeholder="Message"></textarea> */}
-                                </Col>
-                                <button type='submit' className='theme-btn btn-fill form-btn mt-5'>send</button>
+                                <input type="text" placeholder="Name"  value={name} minLength={4} onChange={(e) => setName(e.target.value)} required />
+                                <input type="email" placeholder="Email { someone@example.com }" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                                <input type="phone" placeholder="Phone" value={phone} minLength={10} maxLength={10} onChange={(e) => setPhone(e.target.value)} required />
+                                <input type="text" placeholder="Message" value={message} minLength={4} onChange={(e) => setMessage(e.target.value)} required />
+                                <button className='theme-btn btn-fill form-btn mt-5'>send</button>
                             </form>
                         </div>
                 </Col>
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,47 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import Dentist from '../../../components/Dentist/Dentist';
-import Feature from '../../../components/Feature/Feature';
-import LatestBlog from '../../../components/LatestBlog/LatestBlog';
-import Service from '../../../components/Service/Service';
-import About from '../About/About';
-import Promo from '../Appoinment-promo/Promo';
-import Appoinment from '../Appoinment/Appoinment';
-import Banner from '../Banner/Banner';
-import Gallery from '../Gallery/Gallery';
-import Slick from '../Slick/Slick';
-import Testimonial from '../Testimonial/Testimonial';
-import './Home.css'
+import React, { useEffect, useState } from "react";
+import Dentist from "../../../components/Dentist/Dentist";
+import Feature from "../../../components/Feature/Feature";
+import LatestBlog from "../../../components/LatestBlog/LatestBlog";
+import Service from "../../../components/Service/Service";
+import About from "../About/About";
+import Promo from "../Appoinment-promo/Promo";
+import Appoinment from "../Appoinment/Appoinment";
+import Banner from "../Banner/Banner";
+import Gallery from "../Gallery/Gallery";
+import Slick from "../Slick/Slick";
+import Testimonial from "../Testimonial/Testimonial";
+import "./Home.css";
 
 const Home = () => {
+  const [appoint, setAppoint] = useState(false);
 
-    const [appoint, setAppoint] = useState(false)
+  useEffect(() => {
+    setTimeout(() => {
+      setAppoint(true);
+    }, 5000);
+  }, []);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setAppoint(true)
-        }, 5000)
-    }, [])
-
-    return (
-        <>
-           <Banner />
-           {appoint ?
-            <div className='appoint-container'>
-             <Appoinment onClose={() => setAppoint(!appoint)} />
-            </div> :
-            <div>
-           <Feature />
-           <About />
-           <Service />
-           <Dentist />
-           <Gallery />
-           <Testimonial />
-           <LatestBlog />
-           <Promo />
-           <Slick />
-            </div>}
-        </>
-    );
+  return (
+    <>
+      <Banner />
+      {appoint ? (
+        <div className="appoint-container">
+        <div className="appoint-subcontainer">
+          <Appoinment onClose={() => setAppoint(!appoint)} />
+        </div>
+        </div>
+      ) : (
+        <div>
+          <Feature />
+          <About />
+          <Service />
+          <Dentist />
+          <Gallery />
+          <Testimonial />
+          <LatestBlog />
+          <Promo />
+          <Slick />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Home;
